affafcf615e7144b20ebeaf6ff9276f0b2666099
arindla <ashok.indla@hexagon.com>
2025-02-26 13:50:10 +0530

    angular custom service to fetch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,7 @@ import { CourseCardComponent } from "./course-card/course-card.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
 import { Observable } from "rxjs";
 import { HttpClient, HttpParams } from "@angular/common/http";
+import { CoursesService } from "./services/courses.service";
 
 @Component({
   selector: "app-root",
@@ -23,16 +24,9 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 export class AppComponent implements OnInit {
   courses$: Observable<Course[]>;
 
-  // courses;
+  constructor(private http: HttpClient, private coursesServices: CoursesService) {}
 
-  // HttpClient angular service
-  constructor(private http: HttpClient) {}
-
-  // useEffect for initial render to fetch data
   ngOnInit() {
-    const params = new HttpParams().set("page", "1").set("pageSize", "10");
-
-    this.courses$ = this.http.get<Course[]>("/api/courses", { params }); // get data based on params
-    // this.courses$.subscribe((courses) => (this.courses = courses)); // courses assigned to api response
+    this.courses$ = this.coursesServices.loadCourses();
   }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Course } from "../model/course";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CoursesService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {
+    console.log("Creating the services...");
+  }
+  loadCourses(): Observable<Course[]> {
+    // return this.http.get<Course[]>('/api/courses'); To get all courses
+    const params = new HttpParams().set("page", "1").set("pageSize", "10");
+    return this.http.get<Course[]>("/api/courses", { params });
+  }
 }
