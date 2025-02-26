f1adc03f7e92e1559c6ab3050c8ce3dc70b3aa52
arindla <ashok.indla@hexagon.com>
2025-02-26 13:23:53 +0530

    revert of services initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,6 @@ import { Course } from "./model/course";
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
 import { Observable } from "rxjs";
-import { HttpClient, HttpParams } from "@angular/common/http";
 
 @Component({
   selector: "app-root",
@@ -21,18 +20,9 @@ import { HttpClient, HttpParams } from "@angular/common/http";
   standalone: false,
 })
 export class AppComponent implements OnInit {
-  courses$: Observable<Course[]>;
+  courses = COURSES;
 
-  // courses;
+  constructor() {}
 
-  // HttpClient angular service
-  constructor(private http: HttpClient) {}
-
-  // useEffect for initial render to fetch data
-  ngOnInit() {
-    const params = new HttpParams().set("page", "1").set("pageSize", "10");
-
-    this.courses$ = this.http.get<Course[]>("/api/courses", { params }); // get data based on params
-    // this.courses$.subscribe((courses) => (this.courses = courses)); // courses assigned to api response
-  }
+  ngOnInit() {}
 }
